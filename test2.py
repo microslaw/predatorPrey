a8dc4fa6e38c0f186b97be6c686bbad3c1e09764
microslaw <milosz.grunwald@gmail.com>
2024-03-26 09:34:28 +0100

    add q learning example

--- test2.py
+++ test2.py
@@ -1,7 +1,7 @@
 import gymnasium as gym
 env = gym.make("LunarLander-v2", render_mode="human")
 observation, info = env.reset(seed=42)
-for _ in range(1000):
+for _ in range(100):
    action = env.action_space.sample()  # this is where you would insert your policy
    observation, reward, terminated, truncated, info = env.step(action)
 
@@ -9,3 +9,5 @@ for _ in range(1000):
       observation, info = env.reset()
 
 env.close()
+
+print(env.action_space.sample())
